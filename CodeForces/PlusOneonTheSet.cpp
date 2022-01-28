e892c8ede290a7514ad457524f265da76b795200
Aditya Vishal Singh <vishalsinghaditya526@gmail.com>
2022-01-28 09:22:01 +0530

    Plus One On the set

---
+++ CodeForces/PlusOneonTheSet.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/1624/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int ans = arr[n-1] - arr[0];
+        cout<<ans<<"\n";
+    }
+}
+
